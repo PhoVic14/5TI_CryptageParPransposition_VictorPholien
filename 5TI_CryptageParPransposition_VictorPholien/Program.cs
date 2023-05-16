2264b102198a1ce684c9b8e2d0777c2d95f6f017
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-05-16 09:56:54 +0200

    Ajout du cryptage
    Ajout du cryptage

---
+++ 5TI_CryptageParPransposition_VictorPholien/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _5TI_CryptageParPransposition_VictorPholien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methodes MesOutils = new methodes();
+            Console.WriteLine("Cryptage par transposition");
+            Console.Write("Entrez la clé : ");
+            string cle = Console.ReadLine();
+
+            Console.Write("Entrez le texte à crypter : ");
+            string texte = Console.ReadLine();
+
+            string texteSansEspaces;
+            MesOutils.RetireEspaces(texte, out texteSansEspaces);
+
+            char[,] matrice;
+            MesOutils.DimensionneMat(cle, texteSansEspaces, out matrice);
+
+            MesOutils.EcritChainesDansMat(cle, texteSansEspaces, ref matrice);
+
+            MesOutils.triLigne1(ref matrice);
+
+            char[,] matriceTri;
+            MesOutils.ClasseCle(cle, out matriceTri);
+
+            MesOutils.AttribueRang(ref matrice, ref matriceTri);
+
+            string chaineCryptee;
+            MesOutils.RealiseCrypt(matrice, out chaineCryptee);
+
+            Console.WriteLine("Texte crypté : " + chaineCryptee);
+    }
+
+        
+    }
+}
